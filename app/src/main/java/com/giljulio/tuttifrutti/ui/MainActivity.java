bf1eee73ca88602acc750d45b24f0dca82c0fc3a
Gil Julio <gilsjulio@gmail.com>
2015-08-07 13:45:56 +0100

    Moved UI package

--- app/src/main/java/com/giljulio/tuttifrutti/ui/MainActivity.java
+++ app/src/main/java/com/giljulio/tuttifrutti/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package com.giljulio.tuttifrutti;
+package com.giljulio.tuttifrutti.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.giljulio.tuttifrutti.R;
 
 public class MainActivity extends AppCompatActivity {
 
