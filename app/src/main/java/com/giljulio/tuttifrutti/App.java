a741ae7d72c98edc5e0cbbc6cd52a49293a15efe
Gil Julio <gilsjulio@gmail.com>
2015-08-07 13:40:16 +0100

    Initial commit

---
+++ app/src/main/java/com/giljulio/tuttifrutti/App.java
@@ -0,0 +1,14 @@
+package com.giljulio.tuttifrutti;
+
+import android.app.Application;
+
+/**
+ * Created by Gil on 07/08/15.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
